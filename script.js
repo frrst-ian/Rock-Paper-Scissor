816e89dff27d7133f987a440e3d1a539220a9237
frrst-ian <frrstxd@proton.me>
2024-06-06 17:54:18 +0800

    Added the playGame function and fixed minor bugs
    The play game functions calls the play round function 5 times in a for loop and outputs the score after the round. I also added a if statement in the getHumanChoice function to recursively call the function if the input is invalid.

--- script.js
+++ script.js
@@ -3,32 +3,44 @@ var computerScore = 0;
 
 function getComputerChoice() {
     var computerChoice = ["rock", "paper", "scissor"];
-    var index = Math.floor(Math.random() * computerChoice.length)
+    var index = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[index];
 }
-console.log(getComputerChoice())
-
 function getHumanChoice() {
     var humanChoice = prompt("Enter your choice: Rock/Paper/Scissor").toLowerCase();
+    if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissor") {
+        console.log("Invalid choice. Please enter Rock, Paper, or Scissor.");
+        return getHumanChoice(); // recursively call until valid input is provided
+    }
     return humanChoice;
+}
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        var humanSelection = getHumanChoice();
+        var computerSelection = getComputerChoice();
+        console.log("You chose: " + humanSelection);
+        console.log("Computer chose: " + computerSelection);
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`The final score is:\n Computer: ${computerScore}\n You: ${humanScore}`);
 }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("Tie!");
+        console.log("It's a tie!");
     } else if (
         (humanChoice === "rock" && computerChoice === "scissor") ||
         (humanChoice === "scissor" && computerChoice === "paper") ||
         (humanChoice === "paper" && computerChoice === "rock")
     ) {
-        console.log("You win!");
+        console.log("You win this round!");
         humanScore++;
     } else {
-        console.log("You lose!");
+        console.log("Computer wins this round!");
         computerScore++;
     }
 }
-var humanSelection = getHumanChoice();
-var computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playGame();
