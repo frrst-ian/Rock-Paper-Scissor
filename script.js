71d625af75d5c19e550e6592dffd25c7134ea23a
frrst-ian <frrstxd@proton.me>
2024-06-06 16:59:27 +0800

    Created a function for human choice and rounds
    I used prompt and used conditionals in creating the logic for the game

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-function getComputerChoice () {
-    let computerChoice = ["Rock" , "Paper" , "Scissor"];
+let humanScore = 0;
+let computerScore = 0;
 
-    const index = Math.floor(Math.random() * computerChoice.length )
+function getComputerChoice() {
+    let computerChoice = ["Rock", "Paper", "Scissor"];
+
+    const index = Math.floor(Math.random() * computerChoice.length)
     return computerChoice[index];
 }
-console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice: Rock/Paper/Scissor");
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+
+    if (humanChoice == "rock" && computerChoice == "Rock") {
+        console.log("Tie!");
+    } else if (humanChoice == "paper" && computerChoice == "Rock") {
+        console.log("Tie!");
+    } else if (humanChoice == "scissor" && computerChoice == "Scissor") {
+        console.log("Tie!");
+    } else if (humanChoice == "rock" && computerChoice == "Paper") {
+        console.log("You lose Paper beats Rock");
+        computerScore++;
+    } else if (humanChoice == "scissor" && computerChoice == "Rock") {
+        console.log("You lose Rock beats Scissor");
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "Scissor") {
+        console.log("You lose Scissor beats Paper");
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "Rock") {
+        console.log("You win Paper beats Rock");
+        humanScore++;
+    } else if (humanChoice == "rock" && computerChoice == "Scissor") {
+        console.log("You win Rock beats Scissor");
+        humanScore++;
+    } else if (humanChoice == "scissor" && computerChoice == "Paper") {
+        console.log("You win Scissor beats Paper");
+        humanScore++;
+    } else {
+        console.log("Invalid choice please try again.")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
